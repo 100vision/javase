7d58d75ae99dad0e1dcc0bb90fec0ff0c77320b4
lin <lin.tixiang@outlook>
2021-10-07 23:22:32 +0800

    added Random

---
+++ src/com/bearfamily/Boxing/IntegerTest.java
@@ -0,0 +1,25 @@
+package com.bearfamily.Boxing;
+
+
+/*
+基本类型 ---》 包装类型 （引用类型）
+
+short --> Short （父类Number)
+int --> Integer 父类Number)
+double --> Double  父类Number)
+byte  --> Byte 父类Number)
+long --> Long  父类Number)
+float  --> Float  父类Number)
+char  --> Character
+boolean  --> Boolean
+
+ */
+public class IntegerTest {
+
+    public static void main(String[] args) {
+        java.lang.Integer i = new java.lang.Integer(100);
+        float f = i.floatValue();
+
+
+    }
+}
---
+++ src/com/bearfamily/Random/RandomTest.java
@@ -0,0 +1,4 @@
+package com.bearfamily.Random;
+
+public class RandomTest {
+}
